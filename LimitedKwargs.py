607ca0da39eedc251d6d79004dff7571f60df1ca
RafaelKulig <rafaelkulig@hotmail.com>
2025-08-16 21:55:57 -0300

    Strings included

--- LimitedKwargs.py
+++ LimitedKwargs.py
@@ -1,4 +1,14 @@
 class LimitedKwargs:
+    """
+    A class that accepts a limited set of keyword arguments and maps them to a new dictionary.
+
+    This class only accepts specific keys ('a', 'b', 'c', 'd', 'e', 'f') and maps them to
+    new keys ('g', 'h', 'i', 'j', 'k', 'l'). If any invalid keys are provided, they are ignored,
+    and a message is printed to inform the user of the valid keys.
+    It is useful for ensuring that only a predefined set of arguments is used, while also
+    providing a clear mapping to a different set of keys.
+    """
+
     VALID_KEYS = {'a', 'b', 'c', 'd', 'e', 'f'}
     KEY_MAP = {'a': 'g', 'b': 'h', 'c': 'i', 'd': 'j', 'e': 'k', 'f': 'l'}
 
